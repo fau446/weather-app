634f88f79e140452b6fba0f22d514f312c433aaa
Franklin <Franklinau2@gmail.com>
2022-12-27 11:46:54 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const jui = 78
+
+console.log('hello')
+console.log('Hallk')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBOztBQUVBO0FBQ0Esb0IiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWF0aGVyLWFwcC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBqdWkgPSA3OFxuXG5jb25zb2xlLmxvZygnaGVsbG8nKVxuY29uc29sZS5sb2coJ0hhbGxrJykiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
