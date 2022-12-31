40799f05d6c00147ef3603a34329e6a11ae10169
Franklin <Franklinau2@gmail.com>
2022-12-31 13:54:15 -0800

    Add functions to obtain weather data

---
+++ src/modules/weatherData.js
@@ -0,0 +1,72 @@
+// Weather conditions: clear sky, few clouds, scattered clouds, broken clouds, shower rain, rain, thunderstorm, snow, mist
+
+const weatherData = () => {
+  let data;
+
+  async function getWeatherData(city, country = "") {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=7beebc66d917cf8c09ef1a89928c158d&units=metric`,
+      { mode: "cors" }
+    );
+    data = await response.json();
+
+    console.log(data);
+  }
+
+  function getCurrentTemp() {
+    return data.main.temp;
+  }
+
+  function getFeelsLikeTemp() {
+    return data.main.feels_like;
+  }
+
+  function getMaxTemp() {
+    return data.main.temp_max;
+  }
+
+  function getMinTemp() {
+    return data.main.temp_min;
+  }
+
+  function getHumidity() {
+    return data.main.humidity;
+  }
+
+  function getWeather() {
+    return data.weather[0].description;
+  }
+
+  function getGeneralWeather() {
+    return data.weather[0].main;
+  }
+
+  function getWindSpeed() {
+    return data.wind.speed;
+  }
+
+  function getCityName() {
+    return data.name;
+  }
+
+  function getCountryCode() {
+    return data.sys.country;
+  }
+
+  return {
+    getWeatherData,
+    getCurrentTemp,
+    getFeelsLikeTemp,
+    getMaxTemp,
+    getMinTemp,
+    getHumidity,
+    getWeather,
+    getGeneralWeather,
+    getWindSpeed,
+    getCityName,
+    getCountryCode,
+    data,
+  };
+};
+
+export default weatherData;
