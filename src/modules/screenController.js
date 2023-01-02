68b21c4c95a960badcbe04bab766e055f0736f3b
Franklin <Franklinau2@gmail.com>
2023-01-01 20:23:42 -0800

    Add weather info

--- src/modules/screenController.js
+++ src/modules/screenController.js
@@ -12,6 +12,36 @@ const screenController = () => {
     locationHeader.innerHTML = `${weather.getCityName()}, ${weather.getCountryCode()}`;
   }
 
+  function capatalizeFirstLetter(str) {
+    const newStr = str.split(" ");
+
+    for (let i = 0; i < newStr.length; i++) {
+      newStr[i] = newStr[i].charAt(0).toUpperCase() + newStr[i].slice(1);
+    }
+
+    return newStr.join(" ");
+  }
+
+  function fillWeatherInfo() {
+    const currentTemp = document.querySelector("#current-temp");
+    const weatherCondition = document.querySelector("#weather-condition");
+    const maxTemp = document.querySelector("#max-temp");
+    const minTemp = document.querySelector("#min-temp");
+    const feelsTemp = document.querySelector("#feels-temp");
+    const humidity = document.querySelector("#humidity");
+    const wind = document.querySelector("#wind");
+
+    currentTemp.innerText = `${Math.floor(weather.getCurrentTemp())}\u00B0C`;
+    weatherCondition.innerText = capatalizeFirstLetter(weather.getWeather());
+    maxTemp.innerText = `Max: ${Math.floor(weather.getMaxTemp())}\u00B0C`;
+    minTemp.innerText = `Min: ${Math.floor(weather.getMinTemp())}\u00B0C`;
+    feelsTemp.innerText = `Feels Like: ${Math.floor(
+      weather.getFeelsLikeTemp()
+    )}\u00B0C`;
+    humidity.innerText = `Humidity: ${weather.getHumidity()}%`;
+    wind.innerText = `Wind Speed: ${weather.getWindSpeed()} meters/second`;
+  }
+
   function resetInputField() {
     locationInput.value = "";
   }
@@ -19,6 +49,7 @@ const screenController = () => {
   async function displayData() {
     await weather.getWeatherData(locationInput.value);
     fillLocationHeader();
+    fillWeatherInfo();
     resetInputField();
   }
 
